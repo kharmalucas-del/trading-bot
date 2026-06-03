a039a2cd2d2ca3f1a70d900c327ed725cca99c97
kharmalucas-del <kharmalucas@gmail.com>
2026-06-03 15:00:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import requests
 print("FINAL PRO BOT STARTING...")
 
 # ====== KEYS ======
-api_key = "mx0vgld2XcJBQm9tff"
+api_key = "mx0vgld2XcJBQmtff"
 secret = "bb1b53bf127b474a90909fa83b81be8d"
 
 # ====== TELEGRAM ======
